e8ceed45f06d630e2502eb7a3faaf651d388da50
Abdulrazaq Juma <jumaabdulrazaq7@gmail.com>
2026-06-14 23:22:12 +0400

    Deploy enlarged login logo

--- _next/static/chunks/app/login/page-42682227bc4c283a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[520],{4414:(e,s,_)=>{Promise.resolve().then(_.bind(_,2587))}},e=>{e.O(0,[730,864,154,587,441,255,358],()=>e(e.s=4414)),_N_E=e.O()}]);
